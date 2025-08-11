31661280a843fe05155b3b776b34a6b9bfe636f5
Rowedy413 <rinkudadar1234@gmail.com>
2025-08-11 21:26:53 +0530

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const USERNAME = "admin";
+const PASSWORD = "1234";
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === USERNAME && password === PASSWORD) {
+    // Serve your protected page HTML
+    res.sendFile(__dirname + '/../public/index.html');
+  } else {
+    res.send(`
+      <h1 style="color:red;text-align:center;">Invalid Credentials</h1>
+      <p style="text-align:center;"><a href="/">Go back</a></p>
+    `);
+  }
+});
+
+module.exports = app;
